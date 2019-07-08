d766376f9a31b59216d3341aa80c0ade9df3e828
Eunjae Lee <karis612@gmail.com>
2019-07-08 23:29:14 +0200

    chore: fix script for unit test

--- packages/shipjs-lib/tests/bootstrap-examples/empty.sh
+++ packages/shipjs-lib/tests/bootstrap-examples/empty.sh
@@ -6,5 +6,6 @@ mkdir -p sandbox/$PROJECT_NAME && \
 WORKING_DIRECTORY=`pwd` && \
 cd sandbox/$PROJECT_NAME && \
 npm init -y && \
+replace-in-file '/"version": ".*"/' '"version": "0.0.1"' package.json --isRegex && \
 git init && \
 cd $WORKING_DIRECTORY
