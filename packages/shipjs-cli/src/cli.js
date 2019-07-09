a63e9208002c8868623e941aa2971ade8751dc21
Eunjae Lee <karis612@gmail.com>
2019-07-09 23:15:57 +0200

    chore: remove CLI framework

---
+++ packages/shipjs-cli/src/cli.js
@@ -0,0 +1,11 @@
+import prepare from './flow/prepare';
+
+const flowMap = {
+  prepare,
+};
+
+export function cli(args) {
+  const flowName = args[2];
+  const flow = flowMap[flowName];
+  flow();
+}
---
+++ packages/shipjs-cli/src/flow/prepare.js
@@ -0,0 +1,3 @@
+export default function prepare() {
+  console.log('prepare!');
+}
