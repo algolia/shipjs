150251c0fd447c6781ee2a686b6531138531812a
Eunjae Lee <eunjae.lee@algolia.com>
2019-07-10 16:37:50 +0200

    chore: remove pull.js

--- packages/shipjs-lib/src/index.js
+++ packages/shipjs-lib/src/index.js
@@ -4,4 +4,4 @@ export {
 } from './lib/util/hasTagForCurrentVersion';
 export { default as getNextVersion } from './lib/util/getNextVersion';
 export { default as updateVersion } from './lib/util/updateVersion';
-export { validate } from './lib/prepare';
+export { default as validate } from './lib/prepare/validate';
--- packages/shipjs-lib/src/lib/git/pull.js
+++
@@ -1,5 +0,0 @@
-import exec from '../shell/exec';
-
-export default function pull(dir = '.') {
-  exec('git pull', { dir });
-}
