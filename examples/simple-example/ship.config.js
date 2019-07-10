229fb960d9692796c27a3632ef28adabe784958a
Eunjae Lee <karis612@gmail.com>
2019-07-10 23:18:20 +0200

    chore: remove unused example

--- examples/simple-example/ship.config.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-module.exports = {
-  packageJsons: ['package.json'],
-  versionUpdated(version) {
-    fs.writeFileSync('version.js', `export default "v${version}"\n`);
-  },
-};
--- examples/simple-example/version.js
+++
@@ -1,1 +0,0 @@
-export default "v0.0.1";
