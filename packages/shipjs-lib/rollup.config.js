7440aaf68b8897a5e19a13e2cc3a026e0b56db59
Eunjae Lee <eunjae.lee@algolia.com>
2019-07-16 18:10:51 +0200

    chore: add esm build

--- packages/shipjs-lib/rollup.config.js
+++ packages/shipjs-lib/rollup.config.js
@@ -5,7 +5,10 @@ import pkg from './package.json';
 export default [
   {
     input: 'src/index.js',
-    output: [{ file: pkg.main, format: 'cjs' }],
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' },
+    ],
     external: ['fs', 'path', 'os', 'events', 'assert', 'util'],
     plugins: [resolve({ preferBuiltins: true }), commonjs()],
   },
