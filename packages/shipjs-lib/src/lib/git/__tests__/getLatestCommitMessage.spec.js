88e318da57e97b9433b83b2dff83338a97de4a0f
Eunjae Lee <eunjae.lee@algolia.com>
2019-07-18 13:59:45 +0200

    chore: add bolierplate script

--- packages/shipjs-lib/src/lib/git/__tests__/getLatestCommitMessage.spec.js
+++ packages/shipjs-lib/src/lib/git/__tests__/getLatestCommitMessage.spec.js
@@ -3,8 +3,8 @@ import silentExec from '../../shell/silentExec';
 
 describe('getLatestCommitMessage', () => {
   it('gets latest commit message', () => {
-    silentExec('./tests/bootstrap-examples/lastest-commit-message.sh');
-    const actual = getLatestCommitMessage('sandbox/lastest-commit-message');
-    expect(actual).toBe('this is a lastest commit message');
+    silentExec('./tests/bootstrap-examples/latest-commit-message.sh');
+    const actual = getLatestCommitMessage('sandbox/latest-commit-message');
+    expect(actual).toBe('this is a latest commit message');
   });
 });
