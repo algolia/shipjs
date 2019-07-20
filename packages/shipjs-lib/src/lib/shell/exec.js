8c121a6584e27cf4eff876a0bd46e11d87f08672
shipjs <shipjs@test.com>
2019-07-20 10:29:18 +0200

    fix: exec behavior

--- packages/shipjs-lib/src/lib/shell/exec.js
+++ packages/shipjs-lib/src/lib/shell/exec.js
@@ -1,11 +1,15 @@
 import execa from 'execa';
+import tempWrite from 'temp-write';
+import fs from 'fs';
 
 export default function exec(
   command,
   { dir, ignoreError = false, silent = false } = {}
 ) {
   try {
-    const result = execa.commandSync(command, { cwd: dir });
+    const filePath = tempWrite.sync(command);
+    fs.chmodSync(filePath, '755');
+    const result = execa.sync(command, { shell: true, cwd: dir });
     if (!silent) {
       /* eslint-disable no-console */
       console.log(result.stdout);
