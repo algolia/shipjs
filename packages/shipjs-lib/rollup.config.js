97798c0a55771f0541ae28bbea8f41e4d9d7a3e7
shipjs <shipjs@test.com>
2019-07-21 14:04:15 +0200

    chore: use shelljs instead of execa
    `shelljs` used to break the build, and I've changed to require it instead of importing it.
    Now it works fine. I don't understand what happens in the behind scene in the build process.
    Anyway this way, it's safe to use shelljs which is more suitable than execa.

--- packages/shipjs-lib/rollup.config.js
+++ packages/shipjs-lib/rollup.config.js
@@ -2,37 +2,11 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import pkg from './package.json';
 
-const onwarn = warning => {
-  // Silence circular dependency warning for moment package
-  if (
-    warning.code === 'CIRCULAR_DEPENDENCY' &&
-    [
-      '../../node_modules/glob/glob.js',
-      '../../node_modules/shelljs/shell.js',
-    ].includes(warning.importer)
-  ) {
-    return;
-  }
-
-  // eslint-disable-next-line no-console
-  console.warn(`(!) ${warning.message}`);
-};
-
 export default [
   {
-    onwarn,
     input: 'src/index.js',
     output: [{ file: pkg.main, format: 'es' }],
-    external: [
-      'fs',
-      'path',
-      'os',
-      'events',
-      'assert',
-      'util',
-      'child_process',
-      'stream',
-    ],
+    external: ['fs', 'path', 'url'],
     plugins: [resolve({ preferBuiltins: true }), commonjs()],
   },
 ];
