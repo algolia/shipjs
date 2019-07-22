6f6555241dc15ca8b9909c4a75c05c868b7c05d4
Eunjae Lee <eunjae.lee@algolia.com>
2019-07-22 13:33:46 +0200

    chore: add comment why requiring shelljs (#61)

--- packages/shipjs-lib/src/lib/shell/exec.js
+++ packages/shipjs-lib/src/lib/shell/exec.js
@@ -1,5 +1,18 @@
 // eslint-disable-next-line import/no-commonjs
 const shell = require('shelljs');
+/*
+  This used to be `import shell from 'shelljs'`.
+  For some reason I don't know, it broke the build.
+  
+  `var shellMethods = Object.create(shell);`
+  The line above is from the build output.
+  At the time it was executed, there was no `shell` defined yet.
+  So it threw the following:
+    TypeError: Object prototype may only be an Object or null: undefined
+
+  I tried many different things to fix this, but couldn't find a solution.
+  Using `require` instead of `import` is the only workaround at the moment.
+*/
 
 export default function exec(
   command,
