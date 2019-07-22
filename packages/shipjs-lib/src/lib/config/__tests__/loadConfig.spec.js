e4d856e265ea9666bbfcbd90ecf07e3f7adf6b3b
Eunjae Lee <karis612@gmail.com>
2019-07-23 00:15:01 +0200

    chore: add test for default config

--- packages/shipjs-lib/src/lib/config/__tests__/loadConfig.spec.js
+++ packages/shipjs-lib/src/lib/config/__tests__/loadConfig.spec.js
@@ -1,28 +1,56 @@
-import loadConfig from '../loadConfig';
+import loadConfig, { loadWithUserConfig } from '../loadConfig';
 import defaultConfig from '../defaultConfig';
+import path from 'path';
 
 const baseConfig = expect.objectContaining({
   baseBranches: expect.any(Array),
 });
 
+const d = dirName => path.resolve(__dirname, dirName);
+
 describe('loadConfig', () => {
   it('does not throw when there is no config', () => {
-    expect(loadConfig('non-existing-dir')).toMatchObject(baseConfig);
+    expect(loadConfig(d('non-existing-dir'))).toMatchObject(baseConfig);
   });
 
   it('should load config', () => {
-    expect(loadConfig('test')).toMatchObject(baseConfig);
+    expect(loadConfig(d('example'))).toMatchObject(baseConfig);
   });
 
   it('should get function properly', () => {
-    const config = loadConfig('test');
+    const config = loadConfig(d('example'));
     expect(typeof config.versionUpdated).toBe('function');
   });
+});
 
+describe('loadWithUserConfig', () => {
   it('should merge with defaultConfig', () => {
-    const config = loadConfig('test', 'ship2.config.js');
+    const config = loadWithUserConfig({
+      versionUpdated() {},
+    });
     ['baseBranches', 'conventionalChangelogArgs'].forEach(key => {
       expect(config[key]).toBe(defaultConfig[key]);
     });
+  });
+
+  it('overwrites the children of mergeStrategy', () => {
+    const config = loadWithUserConfig({
+      mergeStrategy: {
+        toSameBranch: undefined,
+        toReleaseBranch: undefined,
+      },
+    });
+    const { mergeStrategy } = config;
+    expect(mergeStrategy.toSameBranch).toBe(undefined);
+    expect(mergeStrategy.toReleaseBranch).toBe(undefined);
+  });
+
+  it('gets default children of mergeStrategy', () => {
+    const config = loadWithUserConfig({
+      mergeStrategy: {},
+    });
+    const { mergeStrategy } = config;
+    expect(mergeStrategy.toSameBranch).toEqual([]);
+    expect(mergeStrategy.toReleaseBranch).toEqual({});
   });
 });
--- packages/shipjs-lib/src/lib/config/loadConfig.js
+++ packages/shipjs-lib/src/lib/config/loadConfig.js
@@ -2,19 +2,21 @@ import { resolve } from 'path';
 import { existsSync } from 'fs';
 import defaultConfig from './defaultConfig';
 
-export default function loadConfig(dir = '.', filename = 'ship.config.js') {
-  const fullPath = resolve(dir, filename);
-  const userConfig = existsSync(fullPath) ? require(fullPath) : {};
+export function loadWithUserConfig(userConfig) {
   const config = {
     ...defaultConfig,
     ...userConfig,
   };
-
   config.mergeStrategy = {
     toSameBranch: [],
     toReleaseBranch: {},
     ...config.mergeStrategy,
   };
-
   return config;
+}
+
+export default function loadConfig(dir = '.', filename = 'ship.config.js') {
+  const fullPath = resolve(dir, filename);
+  const userConfig = existsSync(fullPath) ? require(fullPath) : {};
+  return loadWithUserConfig(userConfig);
 }
