a5d2410758894d2471c427522a58ea1be5160e41
shipjs <shipjs@test.com>
2019-08-02 17:38:47 +0200

    chore: rename cli folder name

--- ship.config.js
+++ ship.config.js
@@ -5,17 +5,17 @@ module.exports = {
   packageJsons: [
     "package.json",
     "packages/shipjs-lib/package.json",
-    "packages/shipjs-cli/package.json"
+    "packages/shipjs/package.json"
   ],
   versionUpdated: ({ version, dir, exec }) => {
     exec(`npx json -I -f lerna.json -e 'this.version = "${version}"'`);
     exec(
-      `npx json -I -f packages/shipjs-cli/package.json -e 'this.dependencies["shipjs-lib"] = "${version}"'`
+      `npx json -I -f packages/shipjs/package.json -e 'this.dependencies["shipjs-lib"] = "${version}"'`
     );
     exec(
-      `echo "export default '${version}';" > packages/shipjs-cli/src/version.js`
+      `echo "export default '${version}';" > packages/shipjs/src/version.js`
     );
   },
   publishCommand: () =>
-    `(cd packages/shipjs-lib && npm publish) && (cd packages/shipjs-cli && npm publish)`
+    `(cd packages/shipjs-lib && npm publish) && (cd packages/shipjs && npm publish)`
 };
