2e4552759d98fdbd314592cb61181d91899275a6
Eunjae Lee <karis612@gmail.com>
2019-08-18 22:54:31 +0200

    fix: fix wrong command for git push

--- packages/shipjs/src/step/push.js
+++ packages/shipjs/src/step/push.js
@@ -2,5 +2,5 @@ import runStep from './runStep';
 
 export default ({ dir, dryRun }) =>
   runStep({ title: 'Pushing to remote.' }, ({ run }) => {
-    run('git pull', dir, dryRun);
+    run('git push', dir, dryRun);
   });
