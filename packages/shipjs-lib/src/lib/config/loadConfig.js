f941bd795a48f5520750bc82470b83536f218c08
Eunjae Lee <eunjae.lee@algolia.com>
2019-08-20 18:24:04 +0200

    feat: accept slack incoming hook from ENV (#200)

--- packages/shipjs-lib/src/lib/config/loadConfig.js
+++ packages/shipjs-lib/src/lib/config/loadConfig.js
@@ -1,6 +1,8 @@
+import dotenv from 'dotenv';
 import { resolve } from 'path';
 import { existsSync } from 'fs';
 import defaultConfig from './defaultConfig';
+dotenv.config();
 
 export function loadWithUserConfig(userConfig) {
   const config = {
@@ -12,6 +14,8 @@ export function loadWithUserConfig(userConfig) {
     toReleaseBranch: {},
     ...config.mergeStrategy,
   };
+  config.slackIncomingHook =
+    process.env.SLACK_INCOMING_HOOK || config.slackIncomingHook;
   return config;
 }
 
