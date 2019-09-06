38eeba9ec93a4ca4589aab7392364a475d571708
shipjs <shipjs@test.com>
2019-09-06 16:44:07 +0200

    fix: validate hub with token

--- packages/shipjs/src/helper/validateBeforePrepare.js
+++ packages/shipjs/src/helper/validateBeforePrepare.js
@@ -16,7 +16,7 @@ export default function validateBeforePrepare({
   if (baseBranches.indexOf(getCurrentBranch(dir)) === -1) {
     result.push(CURRENT_BRANCH_INCORRECT);
   }
-  if (!hasTag(currentTagName)) {
+  if (!hasTag(currentTagName, dir)) {
     result.push(NO_TAG_FOR_CURRENT_VERSION);
   }
   return result.length === 0 ? true : result;
