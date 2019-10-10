8aa57abbc49f1d479439558aced89feb207c346c
Yasuaki Uechi <y@uechi.io>
2019-10-10 21:49:14 +0900

    feat: extract changelog from existing one

--- packages/shipjs/src/step/release/createGitHubRelease.js
+++ packages/shipjs/src/step/release/createGitHubRelease.js
@@ -1,27 +1,66 @@
 import runStep from '../runStep';
 import path from 'path';
+import fs from 'fs';
+import tempWrite from 'temp-write';
+
+function getChangelog(version) {
+  const changelogMatcher = new RegExp(
+    `#+?[\\s\\[]*?(${version})(.|\n)+?(?=#+?[\\s\\[]*?\\d\\.\\d|$)`
+  );
+  const changelogPath = fs.resolve(process.cwd(), 'CHANGELOG.md');
+  try {
+    const changelogFile = fs.readFileSync(changelogPath, 'utf-8').toString();
+    const changelogMatch = changelogFile.match(changelogMatcher);
+    if (changelogMatch !== null) {
+      return changelogMatch[0];
+    }
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      return null;
+    }
+    throw err;
+  }
+  return null;
+}
 
 export default ({ version, config, dir, dryRun }) =>
   runStep({ title: 'Creating the GitHub Release' }, ({ run }) => {
-    if (config.updateChangelog !== true) {
-      return;
-    }
+    if (config.updateChangelog !== true) return;
+
     const { getTagName } = config;
     const tagName = getTagName({ version });
 
-    const args = ['-p angular', '-r 2'].join(' ');
-    const execPath = path.resolve(
-      require.main.filename,
-      '../../node_modules/.bin/conventional-changelog'
-    );
-    const changelog = run(`${execPath} ${args}`, dir, dryRun);
-    const releaseString = `${tagName}
+    const existingChangelog = getChangelog(version);
+    let exportedPath;
+    if (existingChangelog !== null) {
+      // fetch CHANGELOG
+      exportedPath = tempWrite.sync(existingChangelog);
+    } else {
+      // generate CHANGELOG
+      const tmpDir = fs.mkdtempSync('shipjs');
+      exportedPath = path.join(tmpDir, 'changelog');
 
-${dryRun ? '#changelog has been omitted because of dryRun#' : changelog}`;
+      const conventionalChangelog = path.resolve(
+        require.main.filename,
+        '../../node_modules/.bin/conventional-changelog'
+      );
+      const changelogCommand = [
+        conventionalChangelog,
+        '-p angular',
+        '-r 2',
+        `-o ${exportedPath}`,
+      ].join(' ');
 
-    const releaseCommand = `cat <<EOD | hub release create -F - ${tagName}
-${releaseString}
-EOD`;
+      run(changelogCommand, dir, dryRun);
+    }
 
+    // Create GitHub release
+    const releaseCommand = [
+      'hub',
+      'release',
+      'create',
+      `-F ${exportedPath}`,
+      tagName,
+    ].join(' ');
     run(releaseCommand, dir, dryRun);
   });
--- packages/shipjs/src/util/wrapRun.js
+++ packages/shipjs/src/util/wrapRun.js
@@ -13,12 +13,10 @@ export default ({ exec, print, error }) => ({
   if (dryRun) {
     return;
   }
-  const result = exec(command, { dir });
-  if (result.code !== 0) {
+  const { code } = exec(command, { dir });
+  if (code !== 0) {
     print(error('The following command failed:'));
     print(`  > ${command}`);
     process.exit(1); // eslint-disable-line no-process-exit
   }
-  // eslint-disable-next-line consistent-return
-  return result.stdout;
 };
