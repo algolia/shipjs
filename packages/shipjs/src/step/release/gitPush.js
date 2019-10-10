82527cf4bdc4836faf3a7007b2667637a29eacca
Eunjae Lee <eunjae.lee@algolia.com>
2019-10-10 14:00:50 +0200

    fix: do not print github token (#322)

--- packages/shipjs/src/step/release/gitPush.js
+++ packages/shipjs/src/step/release/gitPush.js
@@ -1,38 +1,55 @@
-import { getCurrentBranch, getRepoURLWithToken } from 'shipjs-lib';
+import {
+  getCurrentBranch,
+  getRepoURLWithToken,
+  getRepoURLWithTokenMasked,
+} from 'shipjs-lib';
 import runStep from '../runStep';
 import getBranchNameToMergeBack from '../../helper/getBranchNameToMergeBack';
 
-function getPushCommands({ remote, tagName, dir }) {
+function push({ remote, tagName, run, print, dir, dryRun }) {
   const token = process.env.GITHUB_TOKEN;
   if (token) {
     const url = getRepoURLWithToken(token, remote, dir);
-    return [
-      `git remote add origin-with-token ${url}`,
-      `git push origin-with-token`,
-      `git push origin-with-token ${tagName}`,
-    ];
+    const maskedUrl = getRepoURLWithTokenMasked(remote, dir);
+    print(`    $ git remote add origin-with-token ${maskedUrl}`);
+    run({
+      command: `git remote add origin-with-token ${url}`,
+      dir,
+      dryRun,
+      printCommand: false,
+    });
+    run({ command: `git push origin-with-token`, dir, dryRun });
+    run({ command: `git push origin-with-token ${tagName}`, dir, dryRun });
   } else {
-    return [`git push && git push ${remote} ${tagName}`];
+    run({
+      command: `git push`,
+      dir,
+      dryRun,
+    });
+    run({
+      command: `git push ${remote} ${tagName}`,
+      dir,
+      dryRun,
+    });
   }
 }
 
 export default ({ tagName, config, dir, dryRun }) =>
-  runStep({ title: 'Pushing to the remote.' }, ({ run }) => {
+  runStep({ title: 'Pushing to the remote.' }, ({ run, print }) => {
     const currentBranch = getCurrentBranch(dir);
     const { mergeStrategy, remote } = config;
     const destinationBranch = getBranchNameToMergeBack({
       currentBranch,
       mergeStrategy,
     });
-    const pushCommands = getPushCommands({ remote, tagName, dir });
     if (currentBranch === destinationBranch) {
-      pushCommands.forEach(command => run({ command, dir, dryRun }));
+      push({ remote, tagName, run, print, dir, dryRun });
     } else {
       // currentBranch: 'master'
       // destinationBranch: 'develop'
       // flow: develop -> master -> (here) develop
       run({ command: `git checkout ${destinationBranch}`, dir, dryRun });
       run({ command: `git merge ${currentBranch}`, dir, dryRun });
-      pushCommands.forEach(command => run({ command, dir, dryRun }));
+      push({ remote, tagName, run, print, dir, dryRun });
     }
   });
