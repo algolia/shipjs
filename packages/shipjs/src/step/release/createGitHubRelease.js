ffc0999a12e3ae420eb675ec950b504119f70bf3
Yasuaki Uechi <y@uechi.io>
2019-10-15 14:31:53 +0900

    feat: accept function and glob pattern

--- packages/shipjs/src/step/release/createGitHubRelease.js
+++ packages/shipjs/src/step/release/createGitHubRelease.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import globby from 'globby';
 import tempWrite from 'temp-write';
 
 import runStep from '../runStep';
@@ -26,32 +27,50 @@ function getChangelog(version, rootDir) {
 }
 
 export default ({ version, config, dir, dryRun }) =>
-  runStep({ title: 'Creating a release on GitHub repository' }, ({ run }) => {
-    const { getTagName, release } = config;
-    const tagName = getTagName({ version });
-    const args = [];
+  runStep(
+    { title: 'Creating a release on GitHub repository' },
+    async ({ run }) => {
+      const { getTagName, release } = config;
+      const tagName = getTagName({ version });
+      const args = [];
 
-    // extract matching changelog
-    const changelog = config.updateChangelog
-      ? getChangelog(version, dir)
-      : null;
-    const exportedPath = tempWrite.sync(changelog || tagName);
-    args.push(`-F ${exportedPath}`);
+      // extract matching changelog
+      const changelog = config.updateChangelog
+        ? getChangelog(version, dir)
+        : null;
+      const exportedPath = tempWrite.sync(changelog || tagName);
+      args.push(`-F ${exportedPath}`);
 
-    // handle assets
-    if (
-      release &&
-      Array.isArray(release.assetsToUpload) &&
-      release.assetsToUpload.length > 0
-    ) {
-      for (const asset of release.assetsToUpload) {
-        const assetPath = path.resolve(dir, asset);
-        args.push(`-a ${assetPath}`);
+      // handle assets
+      if (release && release.assetsToUpload) {
+        const option = release.assetsToUpload;
+        const assetPaths = [];
+
+        if (Array.isArray(option) && option.length > 0) {
+          // list
+          for (const asset of option) {
+            const files = await globby(asset);
+            assetPaths.push(...files);
+          }
+        } else if (typeof option === 'function') {
+          // function
+          const files = await Promise.resolve(
+            option({ dir, version, tagName })
+          );
+          assetPaths.push(...files);
+        } else if (typeof option === 'string') {
+          // string
+          assetPaths.push(option);
+        }
+
+        for (const asset of assetPaths) {
+          args.push('-a', asset);
+        }
       }
-    }
 
-    // create GitHub release
-    const hubCommand = ['hub', 'release', 'create'];
-    const command = [...hubCommand, ...args, tagName].join(' ');
-    run({ command, dir, dryRun });
-  });
+      // create GitHub release
+      const hubCommand = ['hub', 'release', 'create'];
+      const command = [...hubCommand, ...args, tagName].join(' ');
+      run({ command, dir, dryRun });
+    }
+  );
