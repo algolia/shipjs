3f0d83ed311fc429906906758e98f02876060dd5
shipjs <shipjs@test.com>
2019-10-18 22:33:22 +0200

    chore: fix broken babel config

--- packages/shipjs/babel.config.js
+++ packages/shipjs/babel.config.js
@@ -1,11 +1,13 @@
 module.exports = {
   presets: [
-    '@babel/preset-env',
-    {
-      targets: {
-        node: 'current',
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
       },
-    },
+    ],
   ],
   plugins: [
     [
