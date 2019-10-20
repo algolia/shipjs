98617b4273e01fef966464c002166614ef533f15
Eunjae Lee <eunjae.lee@algolia.com>
2019-10-20 22:55:45 +0200

    fix: update version at monorepo.mainVersionFile (#351)

--- packages/shipjs-lib/src/lib/util/updateVersion.js
+++ packages/shipjs-lib/src/lib/util/updateVersion.js
@@ -1,8 +1,12 @@
 import { readFileSync, writeFileSync } from 'fs';
 import { resolve } from 'path';
 
-export default function updateVersion(nextVersion, dir = '.') {
-  const filePath = resolve(dir, 'package.json');
+export default function updateVersion({
+  nextVersion,
+  dir = '.',
+  fileName = 'package.json',
+}) {
+  const filePath = resolve(dir, fileName);
   const json = JSON.parse(readFileSync(filePath).toString());
   json.version = nextVersion;
   writeFileSync(filePath, `${JSON.stringify(json, null, 2)}\n`);
