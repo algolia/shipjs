918f52a1432875ffe6f9719c76cf97570e809c63
Eunjae Lee <karis612@gmail.com>
2019-10-20 23:23:21 +0200

    feat: ask questions WIP

--- packages/shipjs/src/step/setup/askQuestions.js
+++ packages/shipjs/src/step/setup/askQuestions.js
@@ -1,5 +1,7 @@
 import inquirer from 'inquirer';
 import { getRemoteBranches } from 'shipjs-lib';
+import fs from 'fs';
+import path from 'path';
 import runStep from '../runStep';
 import { run } from '../../util';
 import { info, reset } from '../../color';
@@ -15,34 +17,106 @@ const formatMessage = (message, description = '') =>
 
 export default async ({ dir }) =>
   await runStep({}, async () => {
-    const branches = getRemoteBranches(dir);
-    const baseBranchCandidate = ['develop', 'dev', 'master'].find(item =>
-      branches.includes(item)
+    const { baseBranch, releaseBranch } = await askBranches(dir);
+    const { configureCircleCI, scheduleCircleCI, cronExpr } = await askCircleCI(
+      dir
     );
-    const releaseBranchCandidate = ['releases', 'release', 'master'].find(
-      item => branches.includes(item)
-    );
-    const { baseBranch, releaseBranch } = await inquirer.prompt([
-      {
-        type: 'list',
-        name: 'baseBranch',
-        choices: branches,
-        message: formatMessage(
-          'What is your base branch?',
-          'This is also called "Default branch".\nYou usually merge pull-requests into this branch.'
-        ),
-        default: baseBranchCandidate,
-      },
-      {
-        type: 'list',
-        name: 'releaseBranch',
-        choices: branches,
-        message: formatMessage(
-          'What is your release branch?',
-          "If you maintain a branch where you keep latest release, choose the branch.\nIf you don't, choose your base branch."
-        ),
-        default: releaseBranchCandidate,
-      },
-    ]);
-    console.log({ baseBranch, releaseBranch });
+    askMonorepo();
+    return {
+      baseBranch,
+      releaseBranch,
+      configureCircleCI,
+      scheduleCircleCI,
+      cronExpr,
+    };
   });
+
+async function askBranches(dir) {
+  const branches = getRemoteBranches(dir);
+  const baseBranchCandidate = ['develop', 'dev', 'master'].find(item =>
+    branches.includes(item)
+  );
+  const releaseBranchCandidate = ['releases', 'release', 'master'].find(item =>
+    branches.includes(item)
+  );
+  const { baseBranch, releaseBranch } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'baseBranch',
+      choices: branches,
+      message: formatMessage(
+        'What is your base branch?',
+        'This is also called "Default branch".\nYou usually merge pull-requests into this branch.'
+      ),
+      default: baseBranchCandidate,
+    },
+    {
+      type: 'list',
+      name: 'releaseBranch',
+      choices: branches,
+      message: formatMessage(
+        'What is your release branch?',
+        "If you maintain a branch where you keep latest release, choose the branch.\nIf you don't, choose your base branch."
+      ),
+      default: releaseBranchCandidate,
+    },
+  ]);
+  return { baseBranch, releaseBranch };
+}
+
+async function askCircleCI(dir) {
+  const circleCIAlreadyExists = fs.existsSync(
+    path.resolve(dir, '.circleci/config.yml')
+  );
+  const { configureCircleCI } = await inquirer.prompt(
+    [
+      !circleCIAlreadyExists
+        ? {
+            type: 'confirm',
+            name: 'configureCircleCI',
+            message: 'Configure CircleCI?',
+            default: true,
+          }
+        : undefined,
+    ].filter(Boolean)
+  );
+  const { scheduleCircleCI } = await inquirer.prompt(
+    [
+      configureCircleCI
+        ? {
+            type: 'confirm',
+            name: 'scheduleCircleCI',
+            message: 'Schedule your release via CircleCI?',
+            default: true,
+          }
+        : undefined,
+    ].filter(Boolean)
+  );
+  const offset = new Date().getTimezoneOffset() / 60;
+  const hour = 11 + offset;
+  const tuesday = 2;
+  const defaultCronExpr = `0 ${hour} * * ${tuesday}`;
+  const { cronExpr } = await inquirer.prompt(
+    [
+      scheduleCircleCI
+        ? {
+            type: 'input',
+            name: 'cronExpr',
+            message: formatMessage(
+              'When to release?',
+              'To learn about cron expressions, visit https://crontab.guru/\nThe default value ("Every Tuesday 11am")'
+            ),
+            default: defaultCronExpr,
+          }
+        : undefined,
+    ].filter(Boolean)
+  );
+
+  return {
+    configureCircleCI,
+    scheduleCircleCI,
+    cronExpr,
+  };
+}
+
+function askMonorepo() {}
