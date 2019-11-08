b2d101a68640cecbd5be496b63efb2711c4bbcd1
Eunjae Lee <karis612@gmail.com>
2019-11-08 22:49:34 +0100

    add global.css

---
+++ website/src/gatsby-theme-docz/index.js
@@ -0,0 +1,21 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import { theme, useConfig, ComponentsProvider } from "docz";
+import { Styled, ThemeProvider } from "theme-ui";
+
+import defaultTheme from "~theme";
+import components from "~components";
+import "./global.css";
+
+const Theme = ({ children }) => {
+  const config = useConfig();
+  return (
+    <ThemeProvider theme={config.themeConfig}>
+      <ComponentsProvider components={components}>
+        <Styled.root>{children}</Styled.root>
+      </ComponentsProvider>
+    </ThemeProvider>
+  );
+};
+
+export default theme(defaultTheme)(Theme);
