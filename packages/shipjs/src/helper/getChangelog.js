35605ecf3fb8e063ad9d9b2da49cc3afbc2caf45
kazuya kawaguchi <kawakazu80@gmail.com>
2019-11-19 00:11:20 +0900

    feat: add extractChangelog to config (#403)
    * feat: add extractChangelog to config
    
    * chore: update functions to have named parameters
    
    * chore: clean up assetsToUpload part
    
    * feat: move extractChanelog to releases and prioritize it over getChangelog

--- packages/shipjs/src/helper/getChangelog.js
+++ packages/shipjs/src/helper/getChangelog.js
@@ -2,11 +2,11 @@ import path from 'path';
 import fs from 'fs';
 import { extractSpecificChangelog } from './';
 
-export default function getChangelog(version, rootDir) {
+export default function getChangelog({ version, rootDir }) {
   const changelogPath = path.resolve(rootDir, 'CHANGELOG.md');
   try {
     const changelogFile = fs.readFileSync(changelogPath, 'utf-8').toString();
-    return extractSpecificChangelog(changelogFile, version);
+    return extractSpecificChangelog({ changelogFile, version });
   } catch (err) {
     if (err.code === 'ENOENT') {
       return null;
