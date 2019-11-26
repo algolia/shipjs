e378e066c1e4f887b6e388a85564d6744eb2ace1
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2019-11-26 16:20:37 +0300

    fix(getChangelog): wrong argument name (#448)

--- packages/shipjs/src/helper/getChangelog.js
+++ packages/shipjs/src/helper/getChangelog.js
@@ -2,8 +2,8 @@ import path from 'path';
 import fs from 'fs';
 import { extractSpecificChangelog } from './';
 
-export default function getChangelog({ version, rootDir }) {
-  const changelogPath = path.resolve(rootDir, 'CHANGELOG.md');
+export default function getChangelog({ version, dir }) {
+  const changelogPath = path.resolve(dir, 'CHANGELOG.md');
   try {
     const changelogFile = fs.readFileSync(changelogPath, 'utf-8').toString();
     return extractSpecificChangelog({ changelogFile, version });
