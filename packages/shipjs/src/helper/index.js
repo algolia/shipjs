f89de0e32088afa0aea84ec2a6422f38d155a750
Eunjae Lee <karis612@gmail.com>
2019-11-28 00:00:22 +0100

    fix: lint error

--- packages/shipjs/src/helper/index.js
+++ packages/shipjs/src/helper/index.js
@@ -1,14 +1,8 @@
 export { default as getBaseBranches } from './getBaseBranches';
-export {
-  default as getBranchNameToMergeBack,
-} from './getBranchNameToMergeBack';
-export {
-  default as getDestinationBranchName,
-} from './getDestinationBranchName';
+export { default as getBranchNameToMergeBack } from './getBranchNameToMergeBack';
+export { default as getDestinationBranchName } from './getDestinationBranchName';
 export { default as validateBeforePrepare } from './validateBeforePrepare';
 export { default as getChangelog } from './getChangelog';
-export {
-  default as extractSpecificChangelog,
-} from './extractSpecificChangelog';
+export { default as extractSpecificChangelog } from './extractSpecificChangelog';
 export { default as gitPush } from './gitPush';
 export { default as runPrettier } from './runPrettier';
