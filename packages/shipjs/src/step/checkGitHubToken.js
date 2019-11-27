f4902dd42d44330a8d9f771c757ce18ea4174c5c
Eunjae Lee <karis612@gmail.com>
2019-11-28 00:19:09 +0100

    fix: guide users to have GITHUB_TOKEN

---
+++ packages/shipjs/src/step/checkGitHubToken.js
@@ -0,0 +1,21 @@
+import runStep from './runStep';
+import { print, exitProcess } from '../util';
+import { error } from '../color';
+
+export default ({ dryRun }) =>
+  runStep({ title: 'Checking GITHUB_TOKEN' }, () => {
+    if (process.env.GITHUB_TOKEN) {
+      return;
+    }
+    print(
+      error('You need to specify GITHUB_TOKEN as an environment variable.')
+    );
+    print('  1. GITHUB_TOKEN=xxx shipjs ...');
+    print('     or');
+    print('  2. Create a file named ".env" and put the following content:');
+    print('     GITHUB_TOKEN=xxx');
+    print('     (".env" should not be committed. Add it to ".gitignore".)');
+    if (!dryRun) {
+      exitProcess(1);
+    }
+  });
