be97690dee7c82d58cd06f868823c8d7f01f27d1
shipjs <shipjs@test.com>
2019-11-27 18:15:46 +0100

    fix: test case

--- packages/shipjs/src/step/prepare/__tests__/createPullRequest.spec.js
+++ packages/shipjs/src/step/prepare/__tests__/createPullRequest.spec.js
@@ -1,9 +1,11 @@
-import { getRepoURL, silentExec } from 'shipjs-lib';
+import { getRepoURL, getRepoInfo, silentExec } from 'shipjs-lib';
 import tempWrite from 'temp-write';
+import Octokit from '@octokit/rest';
 import createPullRequest from '../createPullRequest';
 import { run } from '../../../util';
 import { getDestinationBranchName } from '../../../helper';
 jest.mock('temp-write');
+jest.mock('@octokit/rest');
 
 const getDefaultParams = ({
   currentVersion = '1.2.2',
@@ -11,6 +13,7 @@ const getDefaultParams = ({
   releaseType = 'patch',
   formatPullRequestTitle = () => 'chore: releases v0.1.1',
   formatPullRequestMessage = () => 'chore: releases v0.1.1',
+  noBrowse = true,
   dryRun = false,
 } = {}) => ({
   baseBranch: 'master',
@@ -26,6 +29,7 @@ const getDefaultParams = ({
     remote: 'origin',
     pullRequestReviewer: ['foo', 'bar'],
   },
+  noBrowse,
   dir: '.',
   dryRun,
 });
@@ -33,12 +37,19 @@ const getDefaultParams = ({
 describe('createPullRequest', () => {
   beforeEach(() => {
     getRepoURL.mockImplementationOnce(() => 'https://github.com/my/repo');
+    getRepoInfo.mockImplementationOnce(() => ({
+      owner: 'my',
+      name: 'repo',
+      repo: 'my/repo',
+      branch: 'master',
+      url: 'https://github.com/my/repo',
+    }));
     tempWrite.sync.mockImplementationOnce(() => '/temp/path');
     getDestinationBranchName.mockImplementation(() => 'master');
   });
 
-  it('works in dry mode', () => {
-    const ret = createPullRequest(getDefaultParams({ dryRun: true }));
+  it('works in dry mode', async () => {
+    const ret = await createPullRequest(getDefaultParams({ dryRun: true }));
     expect(run.mock.calls[0][0]).toMatchInlineSnapshot(`
       Object {
         "command": "git remote prune origin",
@@ -46,21 +57,18 @@ describe('createPullRequest', () => {
         "dryRun": true,
       }
     `);
-    expect(run.mock.calls[1][0]).toMatchInlineSnapshot(`
-      Object {
-        "command": "hub pull-request --base master --browse --push --reviewer foo,bar --file /temp/path",
-        "dir": ".",
-        "dryRun": true,
-      }
-    `);
+    expect(Octokit).toHaveBeenCalledTimes(0);
     expect(ret).toEqual({});
   });
 
-  it('returns pr url', () => {
-    silentExec.mockImplementationOnce(() => ({
-      toString: () => `13 chore: releases v0.1.1\n12 docs: update README`,
+  it('returns pr url', async () => {
+    const create = jest.fn().mockImplementationOnce(() => ({
+      data: { number: 13, html_url: 'https://github.com/my/repo/pull/13' }, // eslint-disable-line camelcase
     }));
-    const { pullRequestUrl } = createPullRequest(getDefaultParams());
+    Octokit.mockImplementationOnce(function() {
+      this.pulls = { create, createReviewRequest: jest.fn() };
+    });
+    const { pullRequestUrl } = await createPullRequest(getDefaultParams());
     expect(pullRequestUrl).toEqual('https://github.com/my/repo/pull/13');
   });
 
