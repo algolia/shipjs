efb1704f21b7baf6d469a8c5c0591f16cb0c9f61
Eunjae Lee <eunjae.lee@algolia.com>
2019-12-04 00:17:28 +0100

    fix(config): deep merge `config.slack` (#494)

---
+++ packages/shipjs-lib/src/lib/config/__tests__/mergeConfig.spec.js
@@ -0,0 +1,88 @@
+import mergeConfig from '../mergeConfig';
+
+describe('mergeConfig', () => {
+  it('overwrites the children of mergeStrategy', () => {
+    const config = mergeConfig(
+      {
+        mergeStrategy: {
+          toSameBranch: ['master'],
+        },
+      },
+      {
+        mergeStrategy: {
+          toSameBranch: undefined,
+          toReleaseBranch: undefined,
+        },
+      }
+    );
+    const { mergeStrategy } = config;
+    expect(mergeStrategy.toSameBranch).toBe(undefined);
+    expect(mergeStrategy.toReleaseBranch).toBe(undefined);
+  });
+
+  it('gets default children of mergeStrategy', () => {
+    const config = mergeConfig(
+      {
+        mergeStrategy: {
+          toSameBranch: ['master'],
+        },
+      },
+      {
+        mergeStrategy: {},
+      }
+    );
+    const { mergeStrategy } = config;
+    expect(mergeStrategy.toSameBranch).toEqual([]);
+    expect(mergeStrategy.toReleaseBranch).toEqual({});
+  });
+
+  it('deepmerge', () => {
+    const config = mergeConfig(
+      {
+        slack: {
+          default: {
+            username: 'Ship.js',
+          },
+          prepared: () => {},
+          releaseStart: () => {},
+          releaseSuccess: () => {},
+        },
+      },
+      {
+        slack: {
+          default: {
+            username: 'Release',
+          },
+        },
+      }
+    );
+    expect(config.slack.default.username).toEqual('Release');
+    expect(config.slack.prepared).toBeInstanceOf(Function);
+    expect(config.slack.releaseStart).toBeInstanceOf(Function);
+    expect(config.slack.releaseSuccess).toBeInstanceOf(Function);
+  });
+
+  it('deepmerge (2)', () => {
+    const config = mergeConfig(
+      {
+        slack: {
+          default: {
+            username: 'Ship.js',
+          },
+          prepared: () => {},
+          releaseStart: () => {},
+          releaseSuccess: () => {},
+        },
+      },
+      {
+        slack: {
+          releaseStart: null,
+        },
+      }
+    );
+    expect(config.slack.default.username).toEqual('Ship.js');
+    expect(config.slack.prepared).toBeInstanceOf(Function);
+    expect(config.slack.releaseStart).toBeNull();
+    expect(config.slack.releaseSuccess).toBeInstanceOf(Function);
+  });
+});
