65827c873ffe96f30424dc8cc407f9634da32e50
Yasuaki Uechi <y@uechi.io>
2019-12-24 23:37:57 +0900

    chore: fix typo

--- packages/shipjs/src/step/finished.js
+++ packages/shipjs/src/step/finished.js
@@ -5,12 +5,13 @@ import { print } from '../util';
 export default ({ baseBranch, stagingBranch, pullRequestUrl, dryRun }) =>
   runStep({ title: 'All Finished.' }, () => {
     const prURL = dryRun
-      ? chalk.gray('(Because of --dry-run, pull-request url has been omitted>)')
+      ? chalk.gray('(Because of --dry-run, pull-request url has been omitted)')
       : pullRequestUrl;
     print(`
 You are currently on ${chalk.green.bold(
       stagingBranch
     )} branch, the purpose of which is to make changes to release materials including CHANGELOG.md, and continue to prepare the next release.
+
 ${prURL}
 
 Otherwise, you can safely check out another branch and get back to your development iteration.
