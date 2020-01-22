5d09680cff161000e408a3609aaacbbe0ed44230
shipjs <shipjs@test.com>
2020-01-22 13:45:42 +0100

    fix: clean up all cases for releaseType

--- packages/shipjs-lib/src/lib/util/getReleaseType.js
+++ packages/shipjs-lib/src/lib/util/getReleaseType.js
@@ -1,5 +1,19 @@
-import { diff } from 'semver';
+import { diff, prerelease, minor, patch } from 'semver';
 
 export default function getReleaseType(currentVersion, nextVersion) {
+  if (prerelease(currentVersion) === null && prerelease(nextVersion) !== null) {
+    return 'prerelease';
+  }
+  if (prerelease(currentVersion) !== null && prerelease(nextVersion) === null) {
+    if (patch(nextVersion) === 0) {
+      if (minor(nextVersion) === 0) {
+        return 'major';
+      } else {
+        return 'minor';
+      }
+    } else {
+      return 'patch';
+    }
+  }
   return diff(currentVersion, nextVersion);
 }
