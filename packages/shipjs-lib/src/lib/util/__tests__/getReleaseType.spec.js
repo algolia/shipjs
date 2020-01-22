89e5d732d0ac1d4d2a4750703c583ff82c450cfd
Eunjae Lee <eunjae.lee@algolia.com>
2020-01-22 13:54:09 +0100

    fix: clean up all cases for releaseType (#631)

--- packages/shipjs-lib/src/lib/util/__tests__/getReleaseType.spec.js
+++ packages/shipjs-lib/src/lib/util/__tests__/getReleaseType.spec.js
@@ -1,7 +1,29 @@
 import getReleaseType from '../getReleaseType';
 
 describe('getReleaseType', () => {
-  it('gets release type from current version and next version', () => {
-    expect(getReleaseType('1.0.0', '0.10.99')).toBe('major');
+  it('works', () => {
+    const list = [
+      ['1.2.3', '2.0.0', 'major'],
+      ['1.2.3', '1.3.0', 'minor'],
+      ['1.2.3', '1.2.4', 'patch'],
+      ['1.2.3', '1.2.4-alpha.0', 'prerelease'],
+
+      ['1.2.4-alpha.0', '1.2.4-alpha.1', 'prerelease'],
+      ['1.2.4-alpha.0', '1.2.4', 'patch'],
+      ['1.2.4-alpha.0', '1.2.5', 'patch'],
+      ['1.2.4-alpha.0', '1.3.0', 'minor'],
+      ['1.2.4-alpha.0', '2.0.0', 'major'],
+
+      ['1.3.0-alpha.0', '1.3.0', 'minor'],
+    ];
+
+    list.forEach(([currentVersion, nextVersion, expected]) => {
+      const actual = getReleaseType(currentVersion, nextVersion);
+      expect({ currentVersion, nextVersion, result: actual }).toEqual({
+        currentVersion,
+        nextVersion,
+        result: expected,
+      });
+    });
   });
 });
