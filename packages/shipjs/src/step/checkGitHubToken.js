901fcd8120acd6f6e59dabb4cdb718fc50fe75cf
Haroen Viaene <hello@haroen.me>
2020-02-24 18:36:33 +0100

    feat(error): tell user where to create a GitHub token (#665)
    I was a little confused initially which permissions should be used.
    
    Co-authored-by: Eunjae Lee <karis612@gmail.com>

--- packages/shipjs/src/step/checkGitHubToken.js
+++ packages/shipjs/src/step/checkGitHubToken.js
@@ -10,6 +10,8 @@ export default ({ dryRun }) =>
     print(
       error('You need to specify GITHUB_TOKEN as an environment variable.')
     );
+    print('create a token with `repo` or `public_repo` permissions here:');
+    print('https://github.com/settings/tokens');
     print('  1. GITHUB_TOKEN=xxx shipjs ...');
     print('     or');
     print('  2. Create a file named ".env" and put the following content:');
