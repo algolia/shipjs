f921c5a1be51a8cb1de63442952824152f32ba8b
Eunjae Lee <eunjae.lee@algolia.com>
2020-03-06 13:53:23 +0100

    chore: refine publish commands on PR (#699)
    Co-authored-by: shipjs <shipjs@test.com>

--- packages/shipjs/src/helper/index.js
+++ packages/shipjs/src/helper/index.js
@@ -7,3 +7,4 @@ export { default as extractSpecificChangelog } from './extractSpecificChangelog'
 export { default as gitPush } from './gitPush';
 export { default as runPrettier } from './runPrettier';
 export { default as getPublishCommand } from './getPublishCommand';
+export { default as getPackageDirName } from './getPackageDirName';
