bafce2434ac771dba38dce187d41f483cd626017
shipjs <shipjs@test.com>
2020-03-06 14:32:33 +0100

    fix: allow @(a|b|c) syntax for packagesToBump and packagesToPublish

--- packages/shipjs-lib/src/lib/util/expandPackageList.js
+++ packages/shipjs-lib/src/lib/util/expandPackageList.js
@@ -1,4 +1,4 @@
-import { resolve, join } from 'path';
+import { resolve, join, sep } from 'path';
 import { statSync, readdirSync, existsSync } from 'fs';
 
 const isDirectory = dir => statSync(dir).isDirectory();
@@ -12,12 +12,29 @@ const flatten = arr => arr.reduce((acc, item) => acc.concat(item), []);
 export default function expandPackageList(list, dir = '.') {
   return flatten(
     list.map(item => {
+      const partIndex = item
+        .split(sep)
+        .findIndex(part => part.startsWith('@(') && part.endsWith(')'));
+      if (partIndex !== -1) {
+        const parts = item.split(sep);
+        const part = parts[partIndex];
+        const newList = part
+          .slice(2, part.length - 1)
+          .split('|')
+          .map(subPart => {
+            const newParts = [...parts];
+            newParts[partIndex] = subPart;
+            return newParts.join(sep);
+          });
+        return expandPackageList(newList, dir);
+      }
+
       if (item.endsWith('/*')) {
         const basePath = resolve(dir, item.slice(0, item.length - 2));
-        return getDirectories(basePath).filter(hasPackageJson);
+        return expandPackageList(getDirectories(basePath), dir);
       } else {
         return resolve(dir, item);
       }
     })
-  );
+  ).filter(hasPackageJson);
 }
