4e0257d22dca5df810ade24861b40ac4a7f5c1b1
shipjs <shipjs@test.com>
2020-06-15 00:09:34 +0200

    fix(prepare): return correct releaseType
    It calculate releaseType only with nextVersion

--- packages/shipjs-lib/src/lib/util/__tests__/getReleaseType.spec.js
+++ packages/shipjs-lib/src/lib/util/__tests__/getReleaseType.spec.js
@@ -3,24 +3,16 @@ import getReleaseType from '../getReleaseType';
 describe('getReleaseType', () => {
   it('works', () => {
     const list = [
-      ['1.2.3', '2.0.0', 'major'],
-      ['1.2.3', '1.3.0', 'minor'],
-      ['1.2.3', '1.2.4', 'patch'],
-      ['1.2.3', '1.2.4-alpha.0', 'prerelease'],
-
-      ['1.2.4-alpha.0', '1.2.4-alpha.1', 'prerelease'],
-      ['1.2.4-alpha.0', '1.2.4', 'patch'],
-      ['1.2.4-alpha.0', '1.2.5', 'patch'],
-      ['1.2.4-alpha.0', '1.3.0', 'minor'],
-      ['1.2.4-alpha.0', '2.0.0', 'major'],
-
-      ['1.3.0-alpha.0', '1.3.0', 'minor'],
+      ['2.0.0', 'major'],
+      ['1.3.0', 'minor'],
+      ['1.2.4', 'patch'],
+      ['1.2.5', 'patch'],
+      ['1.2.4-alpha.0', 'prerelease'],
     ];
 
-    list.forEach(([currentVersion, nextVersion, expected]) => {
-      const actual = getReleaseType(currentVersion, nextVersion);
-      expect({ currentVersion, nextVersion, result: actual }).toEqual({
-        currentVersion,
+    list.forEach(([nextVersion, expected]) => {
+      const actual = getReleaseType(nextVersion);
+      expect({ nextVersion, result: actual }).toEqual({
         nextVersion,
         result: expected,
       });
--- packages/shipjs-lib/src/lib/util/getReleaseType.js
+++ packages/shipjs-lib/src/lib/util/getReleaseType.js
@@ -1,19 +1,17 @@
-import { diff, prerelease, minor, patch } from 'semver';
+import { prerelease, minor, patch } from 'semver';
 
-export default function getReleaseType(currentVersion, nextVersion) {
-  if (prerelease(currentVersion) === null && prerelease(nextVersion) !== null) {
+export default function getReleaseType(nextVersion) {
+  if (prerelease(nextVersion) !== null) {
     return 'prerelease';
   }
-  if (prerelease(currentVersion) !== null && prerelease(nextVersion) === null) {
-    if (patch(nextVersion) === 0) {
-      if (minor(nextVersion) === 0) {
-        return 'major';
-      } else {
-        return 'minor';
-      }
+
+  if (patch(nextVersion) === 0) {
+    if (minor(nextVersion) === 0) {
+      return 'major';
     } else {
-      return 'patch';
+      return 'minor';
     }
+  } else {
+    return 'patch';
   }
-  return diff(currentVersion, nextVersion);
 }
