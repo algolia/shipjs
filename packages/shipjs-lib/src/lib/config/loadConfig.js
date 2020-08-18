2b7c586a38fdf604b3c2fe67541d1a619f657af3
jeetiss <jeetiss@yandex.ru>
2020-08-18 12:59:43 +0300

    try load config as cjs and esm files

--- packages/shipjs-lib/src/lib/config/loadConfig.js
+++ packages/shipjs-lib/src/lib/config/loadConfig.js
@@ -1,13 +1,30 @@
 import dotenv from 'dotenv';
 import { resolve } from 'path';
-import { existsSync } from 'fs';
+import { promises, constants } from 'fs';
 
 import defaultConfig from './defaultConfig';
 import mergeConfig from './mergeConfig';
 dotenv.config();
 
-export default function loadConfig(dir = '.', filename = 'ship.config.js') {
-  const fullPath = resolve(dir, filename);
-  const userConfig = existsSync(fullPath) ? require(fullPath) : {};
+const exist = path =>
+  promises
+    .access(path, constants.F_OK)
+    .then(() => path)
+    .catch(() => false);
+
+export default async function loadConfig(
+  dir = '.',
+  filename = 'ship.config',
+  extensions = ['js', 'cjs', 'mjs']
+) {
+  const fullPaths = extensions.map(ext => resolve(dir, `${filename}.${ext}`));
+
+  const fullPath = (await Promise.all(fullPaths.map(path => exist(path)))).find(
+    path => path
+  );
+
+  const userConfig =
+    fullPath && (await exist(fullPath)) ? await import(fullPath) : {};
+
   return mergeConfig(defaultConfig, userConfig);
 }
