7fd161fc7ce998b4806a6fa3461487993ae804e0
jeetiss <jeetiss@yandex.ru>
2020-08-18 13:00:42 +0300

    fix tests

--- packages/shipjs-lib/src/lib/config/__tests__/loadConfig.spec.js
+++ packages/shipjs-lib/src/lib/config/__tests__/loadConfig.spec.js
@@ -4,24 +4,29 @@ import path from 'path';
 
 const baseConfig = expect.objectContaining({});
 
-const d = (dirName) => path.resolve(__dirname, dirName);
+const d = dirName => path.resolve(__dirname, dirName);
 
 describe('loadConfig', () => {
-  it('does not throw when there is no config', () => {
-    expect(loadConfig(d('non-existing-dir'))).toMatchObject(baseConfig);
+  it('does not throw when there is no config', async () => {
+    expect(await loadConfig(d('non-existing-dir'))).toMatchObject(baseConfig);
   });
 
-  it('should load config', () => {
-    expect(loadConfig(d('example'))).toMatchObject(baseConfig);
+  it('should load config', async () => {
+    expect(await loadConfig(d('example'))).toMatchObject(baseConfig);
   });
 
-  it('should get function properly', () => {
-    const config = loadConfig(d('example'));
+  it('should load config with cjs ext', async () => {
+    const config = await loadConfig(d('cjs'));
     expect(typeof config.versionUpdated).toBe('function');
   });
 
-  it('should merge defaultConfig and userConfig', () => {
-    const config = loadConfig(d('example'));
+  it('should get function properly', async () => {
+    const config = await loadConfig(d('example'));
+    expect(typeof config.versionUpdated).toBe('function');
+  });
+
+  it('should merge defaultConfig and userConfig', async () => {
+    const config = await loadConfig(d('example'));
     expect(config.conventionalChangelogArgs).toBe(
       defaultConfig.conventionalChangelogArgs
     );
