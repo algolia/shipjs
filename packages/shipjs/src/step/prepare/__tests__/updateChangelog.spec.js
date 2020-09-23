2c57b65a4a5ccd926ffd2374dddd25e01e5368e6
Anthony Seure <anthony.seure@gmail.com>
2020-09-23 17:05:46 +0200

    feat: understands `--config <FILE>` option in `conventionalChangelogArgs`

---
+++ packages/shipjs/src/step/prepare/__tests__/updateChangelog.spec.js
@@ -0,0 +1,24 @@
+const path = require('path');
+import { prepareParams, parseArgs } from '../updateChangelog';
+import tempWrite from 'temp-write';
+
+describe('prepareParams', () => {
+  it('loads configuration from --config option', () => {
+    const config = {
+      writerOpts: {
+        headerPartial: '## {{version}}',
+      },
+    };
+    const configString = `module.exports = { ${config} };`;
+    const configPath = tempWrite.sync(configString);
+    const configDir = path.basename(path.dirname(configPath));
+
+    const { args } = prepareParams({
+      dir: configDir,
+      conventionalChangelogArgs: `--config ${configPath}`,
+      revisionRange: '1.0.0..1.0.1',
+      reject: () => {},
+    });
+    expect(args.config).toMatch(config);
+  });
+});
