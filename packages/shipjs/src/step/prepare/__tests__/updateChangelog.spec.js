15e03fdb30972ebe06edc90c760a120200a70f0c
shipjs <shipjs@test.com>
2020-09-25 14:48:45 +0200

    fix test case

--- packages/shipjs/src/step/prepare/__tests__/updateChangelog.spec.js
+++ packages/shipjs/src/step/prepare/__tests__/updateChangelog.spec.js
@@ -1,24 +1,26 @@
 const path = require('path');
 import { prepareParams } from '../updateChangelog';
+import { parseArgs } from '../../../util';
 import tempWrite from 'temp-write';
 
 describe('prepareParams', () => {
   it('loads configuration from --config option', () => {
+    parseArgs.mockImplementation(jest.requireActual('../../../util').parseArgs);
     const config = {
       writerOpts: {
         headerPartial: '## {{version}}',
       },
     };
-    const configString = `module.exports = { ${config} };`;
+    const configString = `module.exports = ${JSON.stringify(config)};`;
     const configPath = tempWrite.sync(configString);
     const configDir = path.basename(path.dirname(configPath));
 
     const { args } = prepareParams({
       dir: configDir,
-      conventionalChangelogArgs: `--config ${configPath}`,
+      conventionalChangelogArgs: `-i CHANGELOG.md -s --config ${configPath}`,
       revisionRange: '1.0.0..1.0.1',
       reject: () => {},
     });
-    expect(args.config).toMatch(config);
+    expect(args.config).toEqual(config);
   });
 });
